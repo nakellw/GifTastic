2841185b0468e4e0ef399c7f68c8040e078d6ed4
Nakell Williams <nakellwilliams@FBK-Mac.local>
2018-08-08 11:21:31 -0400

    javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
+
     function gifDisplay() {
         var gif = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=FKIYGHCl5MrA23QTDRB52agaoGe2AI7c";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q="  + gif + "&api_key=FKIYGHCl5MrA23QTDRB52agaoGe2AI7c";
 
 
-        $ajax({
+        https://api.giphy.com/v1/gifs/search?api_key=FKIYGHCl5MrA23QTDRB52agaoGe2AI7c&q=&limit=25&offset=0&rating=G&lang=en
+
+        $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
@@ -33,6 +36,41 @@ $(document).ready(function () {
     };
 
 
+    $('body').on('click', '.giphy', function () {
 
+        var src = $(this).attr("src");
+        if ($(this).hasClass('playing')) {
+            $(this).attr('src', src.replace(/\.gif/i, "_s.gif"));
+        } else {
+            $(this).addClass('playing');
+            $(this).attr('src', src.replace(/\_s.gif/i, ".gif"));
+        }
+    });
 
+var topics = ["cats", "dogs", "fish"];
+
+function gifButtons() {
+    $("#DynamButtons").empty();
+
+    for (var i = 0; i < topics.length; i++) {
+        var b = $("<button>");
+        b.addClass("gif-button");
+        b.addClass("btn")
+        b.attr("data-name", topics[i]);
+        b.text(topics[i]);
+        $("#DynamButtons").append(b);
+    }
+};
+
+$("#form-submit").on("click", function (event) {
+
+    event.preventDefault();
+    var input = $("#form-input").val().trim();
+    topics.push(input);
+    gifButtons();
+});
+
+$(document).on("click", ".gif-button", gifDisplay);
+
+gifButtons();
 });
