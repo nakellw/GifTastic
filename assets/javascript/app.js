872a12fdb9917ffa3e43725494b4d7af0acab398
Nakell Williams <nakellwilliams@FBK-Mac.local>
2018-08-08 13:47:32 -0400

    css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,34 +6,35 @@ $(document).ready(function () {
 
  https://api.giphy.com/v1/gifs/search?api_key=FKIYGHCl5MrA23QTDRB52agaoGe2AI7c&q=&limit=25&offset=0&rating=G&lang=en
 
+                // Pulling the images from Giphy
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             var results = response.data;
-
+                    // Rating of theimages
             for (var i = 0; i < results.length; i++) {
-                var $gifDiv = $("<div class ='gif'>")
-                var rating = results[i].rating;
-
-                var $p = $("<p>").text("Rating: " + rating.toUpperCase());
+                var $gDiv = $("<div class ='gif'>")
+                var rate = results[i].rating; 
+                var $p = $("<p>").text("Rating: " + rate.toUpperCase());
                 $p.addClass("text-center");
                 $p.addClass("gif-rating");
 
                 var $gifImage = $("<img>");
                 $gifImage.addClass("gif-image");
                 $gifImage.attr("src", results[i].images.fixed_height_still.url);
+                    // Posting to the Div
+                $gDiv.append($p);
+                $gDiv.append($gifImage);
 
-                $gifDiv.append($p);
-                $gifDiv.append($gifImage);
-
-                $("#gifDisplay").prepend($gifDiv);
+                $("#gifDisplay").prepend($gDiv);
 
             };
         });
     };
 
 
+        // Play/Pause Feature
     $('body').on('click', '.gif-image', function () {
 
         var src = $(this).attr("src");
@@ -45,26 +46,29 @@ $(document).ready(function () {
         }
     });
 
-var topics = ["basketball", "football", "soccer"];
+
+    // Display Buttons
+var terms = ["basketball", "football", "soccer"];
 
 function gifButtons() {
     $("#Buttons").empty();
 
-    for (var i = 0; i < topics.length; i++) {
+    for (var i = 0; i < terms.length; i++) {
         var b = $("<button>");
         b.addClass("gif-button");
         b.addClass("btn")
-        b.attr("data-name", topics[i]);
-        b.text(topics[i]);
+        b.attr("data-name", terms[i]);
+        b.text(terms[i]);
         $("#Buttons").append(b);
     }
 };
 
+        // Add search term to buttons
 $("#form-submit").on("click", function (event) {
 
     event.preventDefault();
     var input = $("#form-input").val().trim();
-    topics.push(input);
+    terms.push(input);
     gifButtons();
 });
 
